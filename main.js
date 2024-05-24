7ccbaaa3444eac7b6f5c67800d1f57d2d5891ad2
N0XUS0 <153567113+N0XUS0@users.noreply.github.com>
2024-05-24 20:18:15 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $(".customer-logos").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 520,
+          settings: {
+            slidesToShow: 2
+          }
+        }
+      ]
+    });
+  });
+  
+  
+  
+  
